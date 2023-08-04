4bec0faeab9c045b1f89e8916558b3d2b130f8d1
Rob Smith <legendary.belief@gmail.com>
2023-08-03 19:54:36 -0700

    updated routes

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 // to run this script type 'node server.js' in the terminal.
 
 const http = require("http");
+const fs = require("fs");
+
 const PORT = 5000;
+const homePage = fs.readFileSync("index.html");
+const aboutPage = fs.readFileSync("about.html");
+const homePage = fs.readFileSync("index.html");
+const notfoundPage = fs.readFileSync("404.html");
+
 const server = http.createServer((req, res) => {
   if (req.url === "/about") res.end("About Me");
   else if (req.url === "/contact") res.end("Contact Me");
