458af637f621253dbfdd8ee508795780790fa54b
Rob Smith <legendary.belief@gmail.com>
2023-08-03 20:11:11 -0700

    Fixed cammel casing errs

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = 5000;
 const homePage = fs.readFileSync("index.html");
 const aboutPage = fs.readFileSync("about.html");
 const contactPage = fs.readFileSync("contact.html");
-const notfoundPage = fs.readFileSync("404.html");
+const notFoundPage = fs.readFileSync("404.html");
 
 const server = http.createServer((req, res) => {
   if (req.url === "/about") res.end(aboutPage);
@@ -15,7 +15,7 @@ const server = http.createServer((req, res) => {
   else if (req.url === "/") res.end(homePage);
   else {
     res.writeHead(404);
-    res.end(notfoundPage);
+    res.end(notFoundPage);
   }
 });
 
