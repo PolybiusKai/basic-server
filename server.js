a4195eb69c75f641fff4e48a3a6ce151b923f157
Rob Smith <legendary.belief@gmail.com>
2023-08-03 19:31:52 -0700

    Added routes/pages

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
-// to run this script type 'node server.js' in the terminal. 
+// to run this script type 'node server.js' in the terminal.
 
-const http = require('http');
+const http = require("http");
 const server = http.createServer((req, res) => {
-  console.log(req.url)
-  res.end('Hello World');
-})
+  if (req.url === "/about") res.end("About Me");
+  else if (req.url === "/contact") res.end("Contact Me");
+  else if (req.url === "/") res.end("Home");
+  else {
+    res.writeHead(404);
+    res.end("Sorry, page not found");
+  }
+});
 
 server.listen(5500);
 
-console.log('Server running at http://127.0.0.1:5500/');
+console.log("Server running at http://127.0.0.1:5500/");
